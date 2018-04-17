f432d152ee821d8795401eba93a7bd66a2923b7a
Denis Burov <denis.burov@spl.co>
2018-04-17 18:54:33 +0300

    1, 2 and 3 task completed

--- TestApp/ViewModels/MainWindowViewModel.cs
+++ TestApp/ViewModels/MainWindowViewModel.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Xml.Serialization;
 
 using Common.Intefaces;
 
@@ -16,12 +19,20 @@ namespace TestApp.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        public class AverageSalaryData
+        {
+            public string JobName { get; set; }
+
+            public decimal AverageSalary { get; set; }
+        }
+
         private readonly ObservableCollection<PersonViewModel> persons = new ObservableCollection<PersonViewModel>();
         private readonly IPersonRepository personRepository;
 
         private string title = "TestApp";
 
         private DelegateCommand loadPersonsCommand;
+        private DelegateCommand exportJobDataCommand;
 
         private PersonViewModel selectedPerson;
 
@@ -35,6 +46,8 @@ namespace TestApp.ViewModels
         }
 
         public DelegateCommand LoadPersonsCommand => this.loadPersonsCommand ?? (this.loadPersonsCommand = new DelegateCommand(this.LoadPersonExecute));
+
+        public DelegateCommand ExportJobDataCommand => this.exportJobDataCommand ?? (this.exportJobDataCommand = new DelegateCommand(this.ExportJobDataExecute));
 
         public ICollectionView Persons { get; }
 
@@ -55,6 +68,25 @@ namespace TestApp.ViewModels
             List<Person> rawPersons = await this.personRepository.GetAll();
             this.persons.AddRange(rawPersons.Select(p => new PersonViewModel(p)).ToList());
         }
+
+        private async void ExportJobDataExecute()
+        {
+            List<Person> rawPersons = await this.personRepository.GetAll();
+            await this.ExportJobDataAsync(rawPersons);
+        }
+
+        private async Task ExportJobDataAsync(List<Person> rawPersons) => await Task.Run(() =>
+        {
+            var jobAverageSalary = rawPersons.GroupBy(p => p.Job.Name).ToDictionary(g => g.Key, g => g.Average(p => p.Job.Salary));
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(AverageSalaryData));
+            using (FileStream file = File.Create("JobData.xml"))
+            {
+                foreach (var item in jobAverageSalary)
+                {
+                    xmlSerializer.Serialize(file, new AverageSalaryData() { JobName = item.Key, AverageSalary = item.Value });
+                }
+            }
+        });
 
         private void SelectedPersonChangedHandler(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
         {
--- TestApp/ViewModels/PersonViewModel.cs
+++ TestApp/ViewModels/PersonViewModel.cs
@@ -1,5 +1,5 @@
-﻿using Models;
-
+﻿using System;
+using Models;
 using Prism.Mvvm;
 
 namespace TestApp.ViewModels
@@ -8,11 +8,55 @@ namespace TestApp.ViewModels
     {
         private readonly Person person;
 
+        private string firstName;
+        private string lastName;
+        private DateTime birthDay;
+        private string job;
+        private decimal salary;
+        
         public PersonViewModel(Person person)
         {
             this.person = person;
+
+            this.firstName = person.FirstName;
+            this.lastName = person.LastName;
+            this.birthDay = person.BirthDay;
+            this.job = person.Job.Name;
+            this.salary = person.Job.Salary;
         }
 
-        public string Name => $"{this.person.FirstName} {this.person.LastName}";
+        public string FirstName
+        {
+            get => this.firstName;
+            set => this.SetProperty(ref this.firstName, value);
+        }
+
+        public string LastName
+        {
+            get => this.lastName;
+            set => this.SetProperty(ref this.lastName, value);
+        }
+
+        public DateTime BirthDay
+        {
+            get => this.birthDay;
+            set => this.SetProperty(ref this.birthDay, value);
+        }
+
+        public string Job
+        {
+            get => this.job;
+            set => this.SetProperty(ref this.job, value);
+        }
+
+        public decimal Salary
+        {
+            get => this.salary;
+            set => this.SetProperty(ref this.salary, value);
+        }
+
+        public string FullName => $"{this.person.FirstName} {this.person.LastName}";
+
+        public string SalaryText => $"$ {this.salary:0.00}";
     }
 }
